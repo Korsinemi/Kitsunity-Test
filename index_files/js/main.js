8a55741d49ffa9b938c558f2a2977e1430aeb1f9
Kira Itakura <im.kitsunecode@gmail.com>
2021-05-19 10:58:24 -0500

    lets go UwU

---
+++ index_files/js/main.js
@@ -0,0 +1,22 @@
+$(window).scroll(function() {
+    inViewport();
+});
+
+$(window).resize(function() {
+    inViewport();
+});
+
+function inViewport() {
+
+    $('.to-fate-in').each(function() {
+
+        const divPos = $(this).offset().top,
+            topOfWindow = $(window).scrollTop();
+
+        if(divPos < topOfWindow+400){
+            $(this).addClass('visible');
+        }
+
+    });
+
+}
